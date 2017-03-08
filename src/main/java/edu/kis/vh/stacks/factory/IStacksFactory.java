648f1f12fd9b84bb0b3497e5f9882d634b48d8cb
Michał <michbodz@gmail.com>
2017-03-08 22:20:26 +0100

    Zrobiony podpunkt 12

--- src/main/java/edu/kis/vh/stacks/factory/IStacksFactory.java
+++ src/main/java/edu/kis/vh/stacks/factory/IStacksFactory.java
@@ -4,9 +4,9 @@ import edu.kis.vh.stacks.Stack;
 import edu.kis.vh.stacks.StackFIFO;
 import edu.kis.vh.stacks.StackHanoi;
 
-	public interface IStacksFactory {	
-		public Stack getStandardStack();		
-		public Stack getFalseStack();		
-		public StackFIFO getFIFOStack();		
-		public StackHanoi getHanoiStack();		
-	}
+public interface IStacksFactory {	
+	public Stack getStandardStack();		
+	public Stack getFalseStack();		
+	public StackFIFO getFIFOStack();		
+	public StackHanoi getHanoiStack();		
+}
--- src/main/java/edu/kis/vh/stacks/list/StackList.java
+++ src/main/java/edu/kis/vh/stacks/list/StackList.java
@@ -1,7 +1,38 @@
 package edu.kis.vh.stacks.list;
 
 public class StackList {
+	class Node {
+		// zamiast odwolywania sie do danego atrybutu w klasie StackList, uzywane sa teraz gettery, lub settery zwiazane z tym atrybutem
+		private final int value;
+		private Node prev;
+		private Node next;
 
+		public Node(int receivedValue) {
+			value = receivedValue;
+		}
+
+		public int getValue() {
+			return value;
+		}
+
+		public Node getPrev() {
+			return prev;
+		}
+
+		public void setPrev(Node prev) {
+			this.prev = prev;
+		}
+
+		public Node getNext() {
+			return next;
+		}
+
+		public void setNext(Node next) {
+			this.next = next;
+		}
+		
+	}
+	
 	private static final int EMPTY_STACK_VALUE = -1;
 	private Node lastNode;
 

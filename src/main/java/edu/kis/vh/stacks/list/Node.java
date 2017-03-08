b1726bc768da5b637e0a9ad7f4bd826a217b5c5a
Michał <michbodz@gmail.com>
2017-03-08 21:40:24 +0100

    dokonana hermetyzacja nieprywatnych atrybutów, dodany jeden modyfikator private w klasie StackList (wczesniej przeoczony)

--- src/main/java/edu/kis/vh/stacks/list/Node.java
+++ src/main/java/edu/kis/vh/stacks/list/Node.java
@@ -1,12 +1,37 @@
 package edu.kis.vh.stacks.list;
 
 public class Node {
-
-	public int value;
-	public Node prev, next;
+	// zamiast odwolywania sie do danego atrybutu w klasie StackList, uzywane sa teraz gettery, lub settery zwiazane z tym atrybutem
+	private int value;
+	private Node prev;
+	private Node next;
 
 	public Node(int receivedValue) {
 		value = receivedValue;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	public Node getPrev() {
+		return prev;
+	}
+
+	public void setPrev(Node prev) {
+		this.prev = prev;
+	}
+
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
 	}
 	
 }
--- src/main/java/edu/kis/vh/stacks/list/StackList.java
+++ src/main/java/edu/kis/vh/stacks/list/StackList.java
@@ -3,15 +3,15 @@ package edu.kis.vh.stacks.list;
 public class StackList {
 
 	private static final int EMPTY_STACK_VALUE = -1;
-	Node lastNode;
+	private Node lastNode;
 
 	public void pushElement(int elementToPush) {
 		if (lastNode == null)
 			lastNode = new Node(elementToPush);
 		else {
-			lastNode.next = new Node(elementToPush);
-			lastNode.next.prev = lastNode;
-			lastNode = lastNode.next;
+			lastNode.setNext(new Node(elementToPush));
+			lastNode.getNext().setPrev(lastNode);
+			lastNode = lastNode.getNext();
 		}
 	}
 
@@ -26,14 +26,14 @@ public class StackList {
 	public int peek() {
 		if (empty())
 			return EMPTY_STACK_VALUE;
-		return lastNode.value;
+		return lastNode.getValue();
 	}
 
 	public int pop() {
 		if (empty())
 			return EMPTY_STACK_VALUE;
-		int itemRecievedFromPop = lastNode.value;
-		lastNode = lastNode.prev;
+		int itemRecievedFromPop = lastNode.getValue();
+		lastNode = lastNode.getPrev();
 		return itemRecievedFromPop;
 	}
 
